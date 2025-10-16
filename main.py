98b584b7f7774c5dc6bb45d92e3df2b84f1cac68
Hosein (Kasra) Tajali <106269635+Kasra36@users.noreply.github.com>
2025-10-16 16:11:02 +0300

    finish the project

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
 from flask_bootstrap import Bootstrap5
 import requests
 from requests.structures import CaseInsensitiveDict
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 API_KEY = os.getenv("GEOAPIFY_API_KEY")
 GEOAPIFY_ENDPOINT = "https://api.geoapify.com/v2/places"
+GEOAPIFY_ENDPOINT_FORWARD = "https://api.geoapify.com/v1/geocode/search"
 
 HEADERS = CaseInsensitiveDict()
 HEADERS["Accept"] = "application/json"
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
-app.secret_key = "my_super_secret_key_change_this"
+app.secret_key = SECRET_KEY
+
 
 @app.route('/save-location', methods=['POST'])
 def save_location():
@@ -34,8 +37,25 @@ def about():
 
 @app.route('/search')
 def search():
-    
     return render_template('search.html')
+
+@app.route('/search-nearest', methods=['POST'])
+def search_nearest():
+    
+    lat = session.get('lat')
+    lon = session.get('lon')
+
+    params = {
+        "apiKey": API_KEY,
+        "categories": "catering.cafe",
+        "limit": 10,
+        "bias": f"proximity:{lon},{lat}"
+    }
+
+    resp = requests.get(GEOAPIFY_ENDPOINT, headers=HEADERS, params=params)
+    cafes = resp.json().get('features', []) if resp.status_code == 200 else []
+
+    return render_template('search-result.html', cafes=cafes)
 
 @app.route('/search-name', methods=['POST'])
 def search_name():
@@ -57,27 +77,40 @@ def search_name():
 
     return render_template('search-result.html', cafes=cafes)
 
-
-
-@app.route('/search-nearest', methods=['POST'])
-def search_nearest():
+@app.route('/search-loc', methods=['POST'])
+def search_loc():
+    city = request.form.get('city')
+    street = request.form.get('street')
     
-    lat = session.get('lat')
-    lon = session.get('lon')
+    loc_params = {
+        "apiKey": API_KEY,
+        "city": city,
+        "street": street,
+    }
 
+    loc_resp = requests.get(GEOAPIFY_ENDPOINT_FORWARD, headers=HEADERS, params=loc_params)
+    loc_data = loc_resp.json()
+
+    if loc_data.get("features"):
+        lon = loc_data["features"][0]["geometry"]["coordinates"][0]
+        lat = loc_data["features"][0]["geometry"]["coordinates"][1]
+    else:
+        return render_template(
+        'search-result.html',
+        cafes=[],
+        error="No results found for that location.")
+        
     params = {
         "apiKey": API_KEY,
         "categories": "catering.cafe",
-        "limit": 10,
-        "bias": f"proximity:{lon},{lat}"
+        "limit": 20,
+        "bias": f"proximity:{lon},{lat}",
     }
-
+    
     resp = requests.get(GEOAPIFY_ENDPOINT, headers=HEADERS, params=params)
     cafes = resp.json().get('features', []) if resp.status_code == 200 else []
 
     return render_template('search-result.html', cafes=cafes)
-
-
 
 
 if __name__ == '__main__':
